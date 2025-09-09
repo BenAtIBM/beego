e31fef47109c6af1506126837af26579a1ce10f9
Ben Gass <bgass@us.ibm.com>
2025-09-09 11:19:39 -0500

    Min TLS version 1.3

--- server/web/grace/server.go
+++ server/web/grace/server.go
@@ -142,7 +142,8 @@ func (srv *Server) ListenTLS(certFile string, keyFile string) (net.Listener, err
 	}
 	//Force min tls version to 1.2 for Angelica Otero (Mar6 2025) slack stating failing security
 	//scan due to TLS 1.0/1.1 being served.
-	srv.TLSConfig.MinVersion = tls.VersionTLS12
+	srv.TLSConfig.MinVersion = tls.VersionTLS13
+	//srv.TLSConfig.CipherSuites
 	tlsListener := tls.NewListener(tcpKeepAliveListener{ln.(*net.TCPListener)}, srv.TLSConfig)
 	return tlsListener, nil
 }
